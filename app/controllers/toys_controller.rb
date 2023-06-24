41a4e536ed81ac7f07178977f0f7244d9b6f80b2
patiencewanjiru <patiencewanjiru123@gmail.com>
2023-06-24 20:05:23 +0300

    Completed assignment

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -6,26 +6,42 @@ class ToysController < ApplicationController
     render json: toys
   end
 
+  def show
+    toy = find_toy
+    render json: toy
+  end
+
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
-    toy = Toy.find_by(id: params[:id])
+    toy = find_toy
     toy.update(toy_params)
+    render json: toy
   end
 
   def destroy
-    toy = Toy.find_by(id: params[:id])
+    toy = find_toy
     toy.destroy
     head :no_content
   end
 
+  def increment_likes
+    toy = find_toy
+    toy.update(likes: toy.likes + 1)
+    render json: toy
+  end
+
   private
-  
+
   def toy_params
     params.permit(:name, :image, :likes)
+  end
+
+  def find_toy
+    Toy.find(params[:id])
   end
 
 end
